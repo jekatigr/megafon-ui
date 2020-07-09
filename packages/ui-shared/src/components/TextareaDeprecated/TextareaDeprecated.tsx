edac5b4e95d7e4cfde3b6a7d024823de0c77e21d
Gromov Evgeny <evgeniy.gromov@shtormtech.ru>
2020-07-10 01:42:18 +0300

    style((textarea)): test 14

--- packages/ui-shared/src/components/TextareaDeprecated/TextareaDeprecated.tsx
+++ packages/ui-shared/src/components/TextareaDeprecated/TextareaDeprecated.tsx
@@ -5,7 +5,7 @@ import equal from 'deep-equal';
 import { InputLabel, cnCreate, detectTouch } from '@jekatigr/ui-core';
 
 interface ITextareaProps {
-    /** Field title test 4 */
+    /** Field title test 5 */
     label?: React.ReactNode;
     /** Field color scheme */
     color?: 'default' | 'white';
