c1ccdfaf05553584949ea3923ad05fe507239dbf
Gromov Evgeny <evgeniy.gromov@shtormtech.ru>
2020-07-10 00:27:52 +0300

    fix: test 11

--- packages/ui-shared/src/components/TextareaDeprecated/TextareaDeprecated.tsx
+++ packages/ui-shared/src/components/TextareaDeprecated/TextareaDeprecated.tsx
@@ -5,7 +5,7 @@ import equal from 'deep-equal';
 import { InputLabel, cnCreate, detectTouch } from '@jekatigr/ui-core';
 
 interface ITextareaProps {
-    /** Field title test */
+    /** Field title test 2 */
     label?: React.ReactNode;
     /** Field color scheme */
     color?: 'default' | 'white';
